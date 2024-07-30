065cb185e58b8d926b1bb32f2e968fb0edb1bfca
K-manai <130259546+K-manai@users.noreply.github.com>
2024-07-30 23:51:24 +0100

    Add files via upload

---
+++ newfile.py.py
@@ -0,0 +1,12 @@
+# مصفوفة لقياس مؤشرات برامج إدماج الأطفال في خلاف مع القانون بتونس
+child_integration_matrix = [
+    {"program_name": "Program A", "legal_compliance": False, "child_participation_rate": 0.75},
+    {"program_name": "Program B", "legal_compliance": True, "child_participation_rate": 0.90},
+    {"program_name": "Program C", "legal_compliance": False, "child_participation_rate": 0.60},
+    {"program_name": "Program D", "legal_compliance": False, "child_participation_rate": 0.85},
+    # يمكنك إضافة مزيد من برامج الإدماج هنا مع تحديد مدى الامتثال للقانون ونسبة مشاركة الأطفال
+]
+
+# طباعة المصفوفة للتحقق
+for program in child_integration_matrix:
+    print(program)
